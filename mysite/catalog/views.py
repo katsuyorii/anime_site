2c3bbf919c60630936b5633016eea94aafbca18f
katsuyorii <katsuyoriiii@gmail.com>
2023-11-15 11:52:17 +0300

    add pagination

--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -26,6 +26,7 @@ class AnimeListView(ListView):
     model = Anime
     template_name = 'catalog/anime.html'
     context_object_name = 'anime_list'
+    paginate_by = 3
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
