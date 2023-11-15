fad6c902a6c5a569729f19d36f7e472eaf8f9bd8
katsuyorii <katsuyoriiii@gmail.com>
2023-11-15 14:03:34 +0300

    add users app and edit user model

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,5 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
