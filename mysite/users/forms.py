945cbd4928fd2b615140c34228b7119f7a1f087f
katsuyorii <katsuyoriiii@gmail.com>
2023-11-16 15:23:51 +0300

    add registration user logic

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django import forms
 from .models import User
 
@@ -19,4 +19,29 @@ class UserLoginForm(AuthenticationForm):
         fields = [
             'username',
             'password',
+        ]
+
+
+class UserRegistrationForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': "form-control",
+        'placeholder': "Логин",
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': "form-control",
+        'placeholder': "Пароль",
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': "form-control",
+        'placeholder': "Повторите пароль",
+    }))
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'password1',
+            'password2',
         ]
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,11 +1,20 @@
+from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegistrationForm
 from django.urls import reverse_lazy
 
 
 class LoginUserView(LoginView):
     form_class = UserLoginForm
     template_name = 'users/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+
+class RegistrationUserView(CreateView):
+    form_class = UserRegistrationForm
+    template_name = 'users/registration.html'
 
     def get_success_url(self):
         return reverse_lazy('index')
